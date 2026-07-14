4941b3a948a38b6e8ea6685e40c0250b7c9be8e8
C1em5 <112828326+C1em5@users.noreply.github.com>
2026-07-14 22:14:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@
   const RISE_SPEED = 95;       // %/sec while holding
   const FALL_SPEED = 60;       // %/sec gravity
   const FILL_RATE = 16;        // gauge %/sec when fish inside bar
-  const DRAIN_RATE = 26;       // gauge %/sec when fish outside bar
+  const DRAIN_RATE = 1;       // gauge %/sec when fish outside bar
 
   let barPos = 60;    // top position of bar, in % of track height (0-100-BAR_HEIGHT_PCT)
   let fishPos = 80;   // top position of fish center-ish, in %
